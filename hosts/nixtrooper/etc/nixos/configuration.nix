b332d6bde3987a2128f4842b4562c44cd8fb3436
Alex Gherghisan <alexghr@users.noreply.github.com>
2022-01-26 11:53:55 +0000

    host(nixtrooper): switch login from lightdm to gdm

--- hosts/nixtrooper/etc/nixos/configuration.nix
+++ hosts/nixtrooper/etc/nixos/configuration.nix
@@ -101,7 +101,7 @@
     '';
     #displayManager.sddm.enable = true;
     #desktopManager.plasma5.enable = true;
-    displayManager.lightdm.enable = true;
+    displayManager.gdm.enable = true;
     desktopManager.gnome.enable = true;
   };
 
