12ffa023a2327f50ba9fd1d1877f892a125dae6d
Alex Gherghisan <alexghr@users.noreply.github.com>
2022-05-04 08:41:42 +0100

    feat(hk47): enable services.unifi

--- hosts/hk47/configuration.nix
+++ hosts/hk47/configuration.nix
@@ -5,6 +5,14 @@
       ./hardware-configuration.nix
     ];
 
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "21.05"; # Did you read the comment?
+
   boot = {
     kernelParams = ["cma-256M"];
 
@@ -31,37 +39,47 @@
   nixpkgs.config.allowUnfree = true;
   powerManagement.cpuFreqGovernor = "ondemand";
 
-	networking = {
-		hostName = "hk47";
-		networkmanager.enable = false;
-		wireless = {
-			enable = true;
-			interfaces = ["wlan0"];
-		};
-	};
+  networking = {
+    useDHCP = false;
+    hostName = "hk47";
+    networkmanager.enable = false;
+    wireless = {
+      enable = false;
+      interfaces = ["wlan0"];
+    };
 
-	users = {
-		mutableUsers = true;
-		users.ag = {
-			# don't forget to set a password
-			isNormalUser = true;
-			extraGroups = ["wheel"];
-		};
-	};
+    interfaces.eth0.useDHCP = true;
+    interfaces.wlan0.useDHCP = true;
 
-	programs.sway = {
-		enable = true;
-		wrapperFeatures.gtk = true;
-		extraPackages = with pkgs; [
-			swaylock
-			swayidle
-			wl-clipboard
-			alacritty
-			dmenu
-		];
-	};
+    firewall.enable = true;
+    firewall.allowedTCPPorts = [
+      8080 # unifi http port
+      8443 # unifi https port
+    ];
+  };
 
-	environment.systemPackages = with pkgs; [vim raspberrypi-eeprom];
-	services.openssh.enable = true;
+  users = {
+    mutableUsers = true;
+    users.ag = {
+      # don't forget to set a password
+      isNormalUser = true;
+      extraGroups = [ "wheel" ];
+    };
+  };
+
+  environment.systemPackages = with pkgs; [vim raspberrypi-eeprom libraspberrypi];
+
+  services.openssh.enable = true;
+
+  services.unifi = let unifi7 = pkgs.unifi6.overrideAttrs(oldAttrs: rec {
+    src = pkgs.fetchurl {
+      url = "https://dl.ubnt.com/unifi/7.0.25/unifi_sysvinit_all.deb";
+      sha256 = "sha256-DZi2xy6mS3hfqxX1ikiHKPlJ12eaoZVgyl9jKYt91hg=";
+    };
+  }); in {
+    enable = true;
+    unifiPackage = unifi7;
+    openPorts = true;
+  };
 }
 
