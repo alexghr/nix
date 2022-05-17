1e675bd20a5b79c03d76eebd8e1cb6643cd133a0
Alex Gherghisan <alexghr@users.noreply.github.com>
2022-05-17 22:03:01 +0200

    feat(nix-1): add home-manager

--- hosts/nix-1/flake.nix
+++ hosts/nix-1/flake.nix
@@ -3,14 +3,19 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
   inputs.arion.url = "github:hercules-ci/arion/master";
+  inputs.home-manager.url = "github:nix-community/home-manager/release-21.11";
 
-  outputs = { self, nixpkgs, arion }: {
+  outputs = { self, nixpkgs, arion, home-manager }: {
     nixosConfigurations.nix-1 = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
+        home-manager.nixosModule
         arion.nixosModules.arion
         ./configuration.nix
         ({ pkgs, ... }: {
+          home-manager.useUserPackages = true;
+          home-manager.users.ag = import ./home.nix;
+
           virtualisation = {
             podman.enable = true;
             podman.dockerCompat = true;
