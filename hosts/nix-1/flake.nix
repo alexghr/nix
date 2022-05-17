0ee18f7904e085f8aa58ba7263758f36f829e6aa
Alex Gherghisan <alexghr@users.noreply.github.com>
2022-05-17 15:25:32 +0200

    feat(nix-1): add arion

--- hosts/nix-1/flake.nix
+++ hosts/nix-1/flake.nix
@@ -2,11 +2,23 @@
   description = "Manage nix-1";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+  inputs.arion.url = "github:hercules-ci/arion/master";
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, arion }: {
     nixosConfigurations.nix-1 = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      modules = [./configuration.nix];
+      modules = [
+        arion.nixosModules.arion
+        ./configuration.nix
+        ({ pkgs, ... }: {
+          virtualisation = {
+            podman.enable = true;
+            podman.dockerCompat = true;
+            podman.defaultNetwork.dnsname.enable = true;
+          };
+          users.users.ag.extraGroups = ["podman"];
+        })
+      ];
     };
   };
 }
