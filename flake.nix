54db6889f74ae5223d8cf7bd717e252ec52edd60
Alex Gherghisan <alexghr@users.noreply.github.com>
2022-05-31 09:00:39 +0100

    build(flake): lock in release 22.05

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
   description = "Manage my Nix-based machines";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
 
-  inputs.home-manager.url = "github:nix-community/home-manager";
+  inputs.home-manager.url = "github:nix-community/home-manager/release-22.05";
   inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
   inputs.arion.url = "github:hercules-ci/arion/master";
