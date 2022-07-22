0f6f2d0c7b8f1b6c7c9468ea37c8f819a91c64b0
Alex Gherghisan <alexghr@users.noreply.github.com>
2022-07-22 17:38:01 +0100

    fix(ishuttle): fix bad PATH to VSCode

--- hosts/ishuttle/darwin-configuration.nix
+++ hosts/ishuttle/darwin-configuration.nix
@@ -37,7 +37,7 @@
   ];
 
   home-manager.users.ag.programs.bash.bashrcExtra = pkgs.lib.mkAfter ''
-    export PATH="/etc/profiles/per-user/$USER/bin:/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin:$PATH"
+    export PATH="/etc/profiles/per-user/$USER/bin:/Applications/Visual Studio Code.app/Contents/Resources/app/bin:$PATH"
   '';
 
   age.secrets.ag-npmrc = {
