9b0bbef3abbb735071b41e5fe6a7cd2977e31c43
Alex Gherghisan <alexghr@users.noreply.github.com>
2023-06-21 23:07:38 +0100

    refactor(tailscale): make custom tailscale module

--- hosts/vader/configuration.nix
+++ hosts/vader/configuration.nix
@@ -203,37 +203,11 @@
     npmrc = config.age.secrets.ag-npmrc.path;
   };
 
-  age.secrets.tailscale = {
-    file = ../../secrets/vader.tailscale.age;
-  };
-  services.tailscale.enable = true;
-
-  # taken from https://tailscale.com/blog/nixos-minecraft/
-  systemd.services.tailscale-autoconnect = {
-    description = "Automatic connection to Tailscale";
-
-    # make sure tailscale is running before trying to connect to tailscale
-    after = [ "network-pre.target" "tailscale.service" ];
-    wants = [ "network-pre.target" "tailscale.service" ];
-    wantedBy = [ "multi-user.target" ];
-
-    # set this service as a oneshot job
-    serviceConfig.Type = "oneshot";
-
-    # have the job run this shell script
-    script = with pkgs; ''
-      # wait for tailscaled to settle
-      sleep 2
-
-      # check if we are already authenticated to tailscale
-      status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
-      if [ $status = "Running" ]; then # if so, then do nothing
-        exit 0
-      fi
-
-      # otherwise authenticate with tailscale
-      ${tailscale}/bin/tailscale up --auth-key file:${config.age.secrets.tailscale.path} --advertise-exit-node
-    '';
+  age.secrets.tailscale.file = ../../secrets/vader.tailscale.age;
+  alexghr.tailscale = {
+    enable = true;
+    authKeyFile = config.age.secrets.tailscale.path;
+    exitNode = true;
   };
 }
 
