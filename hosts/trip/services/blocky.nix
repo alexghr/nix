3c25126f754eb890f141c6a2dce82c6cdb7185c4
Alex Gherghisan <alexghr@users.noreply.github.com>
2024-03-08 08:50:42 +0000

    feat(trip): replace pi-hole with blocky

---
+++ hosts/trip/services/blocky.nix
@@ -0,0 +1,39 @@
+{
+  config,
+  pkgs,
+  ...
+}:
+{
+  networking.firewall = {
+    allowedTCPPorts = [ 53 ];
+    allowedUDPPorts = [ 53 ];
+  };
+  services.blocky = {
+    enable = true;
+    settings = {
+      upstreams = {
+        groups.default = [
+          "1.1.1.1"
+          "8.8.8.8"
+          "9.9.9.9"
+        ];
+      };
+      blocking = {
+        blackLists.ads = [
+          "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
+        ];
+        clientGroupsBlock.default = ["ads"];
+      };
+
+      caching = {
+        # respect TTL
+        minTime = "0m";
+        maxTime = "0m";
+        prefetching = true;
+
+        # cache NXDOMAIN responses for 5 minutes
+        cacheTimeNegative = "5m";
+      };
+    };
+  };
+}
--- hosts/trip/services/default.nix
+++ hosts/trip/services/default.nix
@@ -1,6 +1,6 @@
 {
   imports = [
-    ./pihole.nix
+    ./blocky.nix
     ./homepage
   ];
 }
