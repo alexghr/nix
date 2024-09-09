f8ea9711eed2f0a9594eafb3c1c8c3817ad2bab7
Alex Gherghisan <alexghr@users.noreply.github.com>
2024-09-09 20:24:37 +0100

    feat(palpatine): update kernel

--- hosts/palpatine/configuration.nix
+++ hosts/palpatine/configuration.nix
@@ -26,7 +26,7 @@
     cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
     nvidia = {
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
+      package = config.boot.kernelPackages.nvidiaPackages.latest;
       powerManagement.enable = true;
       nvidiaSettings = true;
       modesetting.enable = true;
@@ -54,7 +54,7 @@
 
   boot = {
     initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod"];
-    kernelPackages = pkgs.linuxPackages_6_9;
+    kernelPackages = pkgs.linuxPackages;
     kernelModules = ["kvm-amd"];
     loader.efi.efiSysMountPoint = "/boot/efi";
   };
