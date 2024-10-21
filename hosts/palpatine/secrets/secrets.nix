1e9edd47ee9d995d205f2308da9a2c71e1e3517c
Alex Gherghisan <alexghr@users.noreply.github.com>
2024-10-21 08:40:40 +0100

    chore: update ssh keys

--- hosts/palpatine/secrets/secrets.nix
+++ hosts/palpatine/secrets/secrets.nix
@@ -1,6 +1,6 @@
 let
-  alexghrKeys = import ../../alexghr.keys.nix;
-  hostKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICY7KI54Rb+QmhvYZkUTlGzEPni78VfTBFeUno+h1P9K";
+  alexghrKeys = import ../../../alexghr.keys.nix;
+  hostKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICBH453UsbFnTBxcfLeOicu38OBHwwa7eAI0zNyFR6jg";
 in {
   "tailscale.age".publicKeys = alexghrKeys ++ [hostKey];
   "samba.age".publicKeys = alexghrKeys ++ [hostKey];
