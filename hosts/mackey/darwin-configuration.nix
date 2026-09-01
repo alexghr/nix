96980120ea2dcddd59cff99ba95badecff8065b1
Alex Gherghisan <alexghr@users.noreply.github.com>
2026-09-01 10:13:58 +0100

    feat: enable YubiKey ssh keys

--- hosts/mackey/darwin-configuration.nix
+++ hosts/mackey/darwin-configuration.nix
@@ -3,6 +3,23 @@
 
 { config, pkgs, ... }:
 
+let
+    # Codex created this script because there's no askpass program for aarch64-darwin in nixpkgs
+    # As far as I can tell this is safe: it captures the PIN in a hidden text input and returns it
+    # to ssh-agent via stdout (ssh-agent is responsible for creating a secure stdout pipe in this case)
+    # the PIN isn't retained in the script's memory (and ssh-agent zeroes it after it unlocks the YubiKey)
+    macos-ssh-askpass = pkgs.writeShellScriptBin "ssh-askpass" ''
+      exec /usr/bin/osascript - "$1" <<'APPLESCRIPT'
+      on run argv
+        set response to display dialog (item 1 of argv) ¬
+          default answer "" with hidden answer ¬
+          buttons {"Cancel", "OK"} default button "OK"
+        return text returned of response
+      end run
+      APPLESCRIPT
+    '';
+  in
+
 {
   nixpkgs.config.allowUnfree = true;
 
@@ -24,12 +41,16 @@
   environment.systemPackages = with pkgs; [
     pinentry_mac
     gnupg
+    openssh
+    macos-ssh-askpass
   ];
 
   environment.shells = [pkgs.bashInteractive];
 
   home-manager.users.ag.programs.bash.bashrcExtra = pkgs.lib.mkAfter ''
     export PATH="/etc/profiles/per-user/$USER/bin:/Users/$USER/.npm/bin:$PATH"
+    export SSH_ASKPASS="${macos-ssh-askpass}/bin/ssh-askpass";
+    export SSH_ASKPASS_REQUIRE=force
   '';
 
   age.secrets.ag-npmrc = {
