792163bb3ccf0e1d0a740a8704313224d2b5d2b8
Alex Gherghisan <alexghr@users.noreply.github.com>
2026-09-08 21:34:13 +0100

    fix: start home-assistant VM

--- hosts/trip/services/homeassistant/default.nix
+++ hosts/trip/services/homeassistant/default.nix
@@ -6,15 +6,32 @@
 
   systemd.services.homeassistant-vm = {
     description = "Home Assistant Virtual Machine";
-    after = ["libvirtd.service" "network.target"];
-    wants = ["libvirtd.service"];
+    after = ["libvirtd.service" "network-online.target"];
+    wants = ["libvirtd.service" "network-online.target"];
     wantedBy = ["multi-user.target"];
+
+    restartIfChanged = false;
+    stopIfChanged = false;
+
+    script = ''
+      export LC_ALL=C
+      virsh() { ${pkgs.libvirt}/bin/virsh --connect qemu:///system "$@"; }
+
+      virsh net-autostart bridged-network
+      if ! virsh net-list --name | ${pkgs.gnugrep}/bin/grep -Fxq bridged-network; then
+        virsh net-start bridged-network
+      fi
+      if [ "$(virsh domstate homeassistant)" != running ]; then
+        virsh start homeassistant
+      fi
+    '';
 
     serviceConfig = {
       Type = "oneshot";
       RemainAfterExit = true;
-      ExecStart = "${pkgs.libvirt}/bin/virsh start homeassistant || exit 0";
-      ExecStop = "${pkgs.libvirt}/bin/virsh shutdown homeassistant";
+      ExecStop = "${pkgs.libvirt}/bin/virsh --connect qemu:///system shutdown homeassistant";
+      Restart = "on-failure";
+      RestartSec = "10s";
       TimeoutStopSec = "120";
     };
   };
